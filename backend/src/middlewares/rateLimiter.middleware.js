e9aa89e1114c9cd87f7c53be5487f8ca828e0990
KushaagraBhatnagar <kushaagranbhatnagar@gmail.com>
2026-03-08 19:07:29 +0530

    add rate limiter middleware

---
+++ backend/src/middlewares/rateLimiter.middleware.js
@@ -0,0 +1,14 @@
+import rateLimit from "express-rate-limit";
+import { success } from "zod";
+
+export const aiRateLimiter = rateLimit({
+    windowMs: 60*1000, // 1 minute
+    max:20, // each IP ko 20 max req per min
+    message:{
+        success:false,
+        error:"Too many requests to AI. Please try again after a minute."
+    },
+
+    standardHeaders:true,
+    legacyHeaders:false
+})
--- backend/src/routes/interview.routes.js
+++ backend/src/routes/interview.routes.js
@@ -1,11 +1,12 @@
 import express from "express"
 import createSession, { checkAnswer, generateQuestion, continueInterview } from "../controllers/interview.controller.js"
 import { generateInterviewReport } from "../controllers/interview.analytics.controller.js"
+import { aiRateLimiter } from "../middlewares/rateLimiter.middleware.js"
 const router = express.Router()
 
 router.post("/create",createSession)
-router.post("/generate-question",generateQuestion)
-router.post("/submit-answer", checkAnswer);
-router.post("/continue-interview", continueInterview);
+router.post("/generate-question",aiRateLimiter,generateQuestion)
+router.post("/submit-answer",aiRateLimiter, checkAnswer);
+router.post("/continue-interview", aiRateLimiter, continueInterview);
 router.get("/report/:sessionId", generateInterviewReport)
 export default router
